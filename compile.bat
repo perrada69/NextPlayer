02d36bdbc777fb3863194e655ba18b64eee5f0fe
perrada69 <bursa@kchpbt.cz>
2020-05-24 22:25:50 +0200

    Přejmenování souborů

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
-sjasmplus.exe pt3.asm --lst=pt3.lst --sym=pt3.sym.txt
+sjasmplus.exe NextPlayer.asm --lst=NextPlayer.lst --sym=NextPlayer.sym.txt
 e:\cspect\hdfmonkey.exe put e:\cspect\cspect-next-2gb.img NextPlayer.nex player/
 e:\cspect\CSpect.exe -zxnext -basickeys -nextrom -map=player.map -tv -mmc=e:\cspect\cspect-next-2gb.img 
